547c070b81dc45ac65a6e4777509cc8535c03735
Jack hosking <jackhosking2@outlook.com>
2019-08-18 23:00:54 +1200

    Changes to Cart
    TODO: remove overflow of navbar

--- src/views/Home.js
+++ src/views/Home.js
@@ -19,7 +19,16 @@ export class Home extends Component {
                     "name": "Classic tee",
                     "price": 75,
                     "size": "S",
-                    "quantity": 4,
+                    "quantity": 1,
+                    "image": "https://www.partsware.com.au/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/t/mtk002a.jpg",
+                    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                },
+                {
+                    "id": 2,
+                    "name": "Classic tee",
+                    "price": 75,
+                    "size": "L",
+                    "quantity": 3,
                     "image": "https://www.partsware.com.au/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/t/mtk002a.jpg",
                     "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 },
@@ -71,12 +80,13 @@ export class Home extends Component {
             <div>
                 <div>
                     <nav className="nav navbar-color">
-                        <div className="cart-holder">
+                        <div className="cart-header">
                             <button className="cart-button" onClick={() => this.handleCart()}> My Cart ( {quantity} ) </button>
+                            <div>{this._renderCartState(cart)}</div>
                         </div>
                     </nav>
+
                 </div>
-                {this._renderCartState(cart)}
                     {product}
             </div>
         );
@@ -88,7 +98,7 @@ export class Home extends Component {
         let content = false;
         if(this.state.cartVisible){
             content = (
-                <ul>{cart}</ul>
+                <div className="cart-holder">{cart}</div>
             )
         } else {
             content = (
